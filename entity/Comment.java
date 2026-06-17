15d4b5bfd69d80aced1bbf23bd35a114fb223117
Jitendra Kumar Sharma <136931188+Jitendra-Sharma726@users.noreply.github.com>
2026-06-17 16:05:33 +0530

    Create Comment.java

---
+++ entity/Comment.java
@@ -0,0 +1,23 @@
+package entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "blogpost_id")
+    @JsonIgnore
+    private BlogPost blogPost;
+}
